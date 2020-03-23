eceb24cf74222b54793c4d203c0f0a4e9d21d1ca
Eric Schulte <eschulte@grammatech.com>
2020-03-23 15:04:19 -0400

    Adding Common Lisp implementation

---
+++ gtirb-stack-stamp.asd
@@ -0,0 +1,17 @@
+(defsystem "gtirb-stack-stamp"
+    :name "gtirb-stack-stamp"
+    :author "GrammaTech"
+    :licence "MIT"
+    :description "Stack-stamp binary executable transformation over GTIRB"
+    :long-description "Stack stamping is a binary hardening
+transformation in which a random key is xor'd on the return address on
+the top of the stack upon entry to any function.  This same key is
+again xor'd against the top of the stack just before the function
+returns.  This can help defeat return oriented programming (ROP)
+attacks--especially when used as part of a moving target defense
+system."
+    :depends-on (:gtirb-stack-stamp/gtirb-stack-stamp)
+    :class :package-inferred-system
+    :defsystem-depends-on (:asdf-package-system)
+    :perform
+    (test-op (o c) (symbol-call :gtirb-stack-stamp '#:test)))
---
+++ gtirb-stack-stamp.lisp
@@ -0,0 +1,80 @@
+(defpackage :gtirb-stack-stamp/gtirb-stack-stamp
+  (:nicknames :gtirb-stack-stamp)
+  (:use :gt :gtirb :capstone/clos :keystone/clos :stefil)
+  (:import-from :cl-intbytes :int->octets :octets->uint)
+  (:shadow :version :size :architecture :mode :symbol :address :bytes)
+  (:export :gtirb-stack-stamp))
+(in-package :gtirb-stack-stamp/gtirb-stack-stamp)
+(in-readtable :curry-compose-reader-macros)
+
+
+;;; Implementation
+(defun stamp-value (function)
+  "Just hash the function itself to get a deterministic stamp value."
+  (sxhash function))
+
+(defvar *cs* (make-instance 'capstone-engine :architecture :x86 :mode :64)
+  "Capstone engine instance.")
+(defvar *ks* (make-instance 'keystone-engine :architecture :x86 :mode :64)
+  "Keystone engine instance.")
+(set-option *ks* :syntax :syntax-att)
+
+(defgeneric stack-stamp (object)
+  (:documentation "Apply the stack-stamp transformation to OBJECT.")
+  (:method ((obj gtirb)) (mapc #'stack-stamp (modules obj)))
+  (:method ((obj module)) (mapc #'stack-stamp (functions obj)))
+  (:method ((obj func))
+    (unless (exits obj)
+      (warn "Skipping function without exits: ~s" obj)
+      (return-from stack-stamp))
+    (unless (entries obj)
+      (warn "Skipping function without entries: ~s" obj)
+      (return-from stack-stamp))
+    (let* ((key (int->octets (stamp-value obj) 8))
+           (stamp-bytes (asm *ks* (format nil "xorl $0x~x,(%rsp); ~
+                                               xorl $0x~x,4(%rsp);"
+                                          (octets->uint (subseq key 0 4) 4)
+                                          (octets->uint (subseq key 4) 4)))))
+      (mapc (lambda (entry-block)
+              (setf (gtirb:bytes entry-block)
+                    (concatenate 'vector
+                                 stamp-bytes (gtirb:bytes entry-block))))
+            (entries obj))
+      (mapc (lambda (return-block)
+              (let ((bytes (gtirb:bytes return-block)))
+                (if-let ((return-position (position-if [{eql :ret} #'mnemonic]
+                                                       (disasm *cs* bytes))))
+                  (setf (gtirb:bytes return-block)
+                        (concatenate 'vector
+                                     (subseq bytes 0 return-position)
+                                     stamp-bytes
+                                     (subseq bytes return-position))))))
+            (returns obj)))))
+
+
+;;;; Helpers
+(defgeneric instructions (object)
+  (:documentation "Return decoded instructions for OBJECT.")
+  (:method ((obj gtirb-byte-block)) (disasm *cs* (gtirb:bytes obj))))
+
+
+;;;; Main test suite.
+(defsuite test)
+(in-suite test)
+
+(defvar *hello*)
+
+(defvar *base-dir* (nest (make-pathname :directory)
+                         (pathname-directory)
+                         #.(or *compile-file-truename*
+                               *load-truename*
+                               *default-pathname-defaults*)))
+
+(defixture hello
+  (:setup (setf *hello* (read-gtirb (merge-pathnames "tests/hello.v1.gtirb"
+                                                     *base-dir*))))
+  (:teardown (setf *hello* nil)))
+
+(deftest stack-stamp-hello ()
+  (with-fixture hello
+    (stack-stamp *hello*)))
