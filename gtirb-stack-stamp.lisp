657e6a55456fae6dece0509116202d162c9093d4
Eric Schulte <eschulte@grammatech.com>
2020-03-25 19:09:41 -0400

    Switch to using CL GTIRB utility libraries

--- gtirb-stack-stamp.lisp
+++ gtirb-stack-stamp.lisp
@@ -10,7 +10,7 @@
 ;;; endorsement should be inferred.
 (defpackage :gtirb-stack-stamp/gtirb-stack-stamp
   (:nicknames :gtirb-stack-stamp)
-  (:use :gt :gtirb :gtirb/functions :capstone/clos :keystone/clos :stefil)
+  (:use :gt :gtirb :gtirb-functions :gtirb-capstone :stefil)
   (:import-from :cl-intbytes :int->octets :octets->uint)
   (:shadow :version :size :architecture :mode :symbol :address :bytes)
   (:export :gtirb-stack-stamp))
@@ -19,19 +19,11 @@
 
 
 ;;; Implementation
-(defun stamp-value (function)
-  "Just hash the function itself to get a deterministic stamp value."
-  (sxhash function))
-
-(defvar *cs* (make-instance 'capstone-engine :architecture :x86 :mode :64)
-  "Capstone engine instance.")
-(defvar *ks* (make-instance 'keystone-engine :architecture :x86 :mode :64)
-  "Keystone engine instance.")
-(set-option *ks* :syntax :syntax-att)
-
 (defgeneric stack-stamp (object)
-  (:documentation "Apply the stack-stamp transformation to OBJECT.")
-  (:method ((obj gtirb)) (mapc #'stack-stamp (modules obj)))
+  (:documentation "Apply the stack-stamp transformation.")
+  (:method ((obj gtirb))
+    (set-syntax obj :syntax-att)
+    (mapc #'stack-stamp (modules obj)))
   (:method ((obj module)) (mapc #'stack-stamp (functions obj)))
   (:method ((obj func))
     (unless (exits obj)
@@ -40,11 +32,11 @@
     (unless (entries obj)
       (warn "Skipping function without entries: ~s" obj)
       (return-from stack-stamp))
-    (let* ((key (int->octets (stamp-value obj) 8))
-           (stamp-bytes (asm *ks* (format nil "xorl $0x~x,(%rsp); ~
-                                               xorl $0x~x,4(%rsp);"
-                                          (octets->uint (subseq key 0 4) 4)
-                                          (octets->uint (subseq key 4) 4)))))
+    (let* ((key (int->octets (sxhash obj) 8))
+           (stamp-bytes (asm obj (format nil "xorl $0x~x,(%rsp); ~
+                                              xorl $0x~x,4(%rsp);"
+                                         (octets->uint (subseq key 0 4) 4)
+                                         (octets->uint (subseq key 4) 4)))))
       (mapc (lambda (entry-block)
               (setf (gtirb:bytes entry-block)
                     (concatenate 'vector
@@ -60,12 +52,6 @@
                                      stamp-bytes
                                      (subseq bytes return-position))))))
             (returns obj)))))
-
-
-;;;; Helpers
-(defgeneric instructions (object)
-  (:documentation "Return decoded instructions for OBJECT.")
-  (:method ((obj gtirb-byte-block)) (disasm *cs* (gtirb:bytes obj))))
 
 
 ;;;; Main test suite.
