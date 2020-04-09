3e3faa863530e4eed3c756a8220922173112a216
Bill LeVine <blevine@grammatech.com>
2020-04-09 11:45:37 -0400

    Formatting

--- test/test_stack_stamp.py
+++ test/test_stack_stamp.py
@@ -1,14 +1,13 @@
 import unittest
 import subprocess
 
-class StackStampTest(unittest.TestCase):
 
+class StackStampTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        args = ['make', 'factorial']
+        args = ["make", "factorial"]
         ec = subprocess.call(args)
-        self.assertEqual(ec, 0)
+        return ec == 0
 
     def test_1_invocation(self):
         return True
-
