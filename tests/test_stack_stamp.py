0de67bf1d08618d336fc9e786bc4ca46289440f3
Bill LeVine <blevine@grammatech.com>
2020-04-09 15:58:52 -0400

    Update tests

--- tests/test_stack_stamp.py
+++ tests/test_stack_stamp.py
@@ -1,13 +1,18 @@
 import unittest
 import subprocess
+import os
 
 
 class StackStampTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        args = ["make", "factorial"]
+        args = ["make", "-C", "tests", "factorial"]
         ec = subprocess.call(args)
         return ec == 0
+
+    def setUp(self):
+        self._test_file = os.path.join("dist", "factorial")
+        self.assertTrue(os.path.isfile(self._test_file))
 
     def test_1_invocation(self):
         return True
