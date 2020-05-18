de78a5b3c87f5f73d2854bfbde59948845b2e477
Joshua Robbins <jrobbins@grammatech.com>
2020-05-18 16:00:40 -0400

    Added impl for insertInstructions

--- include/gtirb_stack_stamp/gtirb_stack_stamp.hpp
+++ include/gtirb_stack_stamp/gtirb_stack_stamp.hpp
@@ -6,9 +6,9 @@
 #include <string>
 
 namespace gtirb_stack_stamp {
-class RewritingContext {
+class StackStamper {
 public:
-  RewritingContext() {
+  StackStamper() {
     [[maybe_unused]] auto CSRet = cs_open(CS_ARCH_X86, CS_MODE_64, &Capstone);
     assert(CSRet == CS_ERR_OK);
     cs_option(Capstone, CS_OPT_DETAIL, CS_OPT_ON);
@@ -19,12 +19,12 @@ public:
     ks_option(Keystone, KS_OPT_SYNTAX, KS_OPT_SYNTAX_ATT);
   }
 
-  ~RewritingContext() {
+  ~StackStamper() {
     cs_close(&Capstone);
     ks_close(Keystone);
   }
 
-  void insertInstructions(gtirb::ByteInterval& BI, uint64_t offset,
+  void insertInstructions(gtirb::ByteInterval& BI, uint64_t Offset,
                           const std::string& InsnsStr);
   void stackStampEntranceBlock(gtirb::CodeBlock& Block);
   void stackStampExitBlock(gtirb::CodeBlock& Block);
--- src/gtirb_stack_stamp.cpp
+++ src/gtirb_stack_stamp.cpp
@@ -1,1 +1,73 @@
 #include <gtirb_stack_stamp/gtirb_stack_stamp.hpp>
+
+template <typename BlockType>
+static void modifyBlock(BlockType& Block, uint64_t Offset, uint64_t Size) {
+  auto BlockOff = Block.getOffset();
+  auto BlockSize = Block.getSize();
+
+  if (BlockOff <= Offset && BlockOff + BlockSize < Offset) {
+    // increase in size any blocks that intersect with the new bytes
+    Block.setSize(BlockSize + Size);
+  } else if (BlockOff >= Offset) {
+    // move any blocks over that occur after the inserted bytes
+    Block.getByteInterval()->addBlock<BlockType>(BlockOff + Size, &Block);
+  }
+}
+
+void gtirb_stack_stamp::StackStamper::insertInstructions(
+    gtirb::ByteInterval& BI, uint64_t Offset, const std::string& InsnsStr) {
+  gtirb::Addr Addr{0};
+  if (auto BiAddr = BI.getAddress()) {
+    Addr = *BiAddr + Offset;
+  }
+
+  unsigned char* Bytes;
+  size_t BytesLen, StatCount;
+  auto KSRes = ks_asm(Keystone, InsnsStr.c_str(), static_cast<uint64_t>(Addr),
+                      &Bytes, &BytesLen, &StatCount);
+  assert(KSRes == 0);
+
+  // modify contents
+  BI.insertBytes<unsigned char>(BI.bytes_begin<unsigned char>() + Offset, Bytes,
+                                Bytes + BytesLen);
+
+  // modify blocks
+  std::vector<gtirb::CodeBlock*> CodeBlocks;
+  for (auto& Block : BI.code_blocks()) {
+    CodeBlocks.push_back(&Block);
+  }
+  for (auto* Block : CodeBlocks) {
+    modifyBlock(*Block, Offset, BytesLen);
+  }
+
+  std::vector<gtirb::DataBlock*> DataBlocks;
+  for (auto& Block : BI.data_blocks()) {
+    DataBlocks.push_back(&Block);
+  }
+  for (auto* Block : DataBlocks) {
+    modifyBlock(*Block, Offset, BytesLen);
+  }
+
+  // modify symbolic expressions
+  std::vector<gtirb::ByteInterval::SymbolicExpressionElement> SEEs{
+      BI.symbolic_expressions_begin(), BI.symbolic_expressions_end()};
+  for (auto& SEE : SEEs {
+    if (SEE.getOffset() >= Offset) {
+      BI.removeSymbolicExpression(Offset);
+      BI.addSymbolicExpression(Offset + BytesLen, SEE.getSymbolicExpression());
+    }
+  }
+}
+
+void gtirb_stack_stamp::StackStamper::stackStampEntranceBlock(
+    gtirb::CodeBlock& Block) {}
+
+void gtirb_stack_stamp::StackStamper::stackStampExitBlock(
+    gtirb::CodeBlock& Block) {}
+
+void gtirb_stack_stamp::StackStamper::stackStampFunction(
+    gtirb::Module& M, const gtirb::UUID& FunctionId) {}
+
+void gtirb_stack_stamp::stackStamp(gtirb::Module& M) {}
+
+void gtirb_stack_stamp::registerAuxDataSchema() {}
