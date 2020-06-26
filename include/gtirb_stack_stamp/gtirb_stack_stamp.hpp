2f94080d867b5d311a51da3083d4cfb44c46b485
Joshua Robbins <jrobbins@grammatech.com>
2020-06-26 16:02:10 -0400

    Added documentation for C++ API

--- include/gtirb_stack_stamp/gtirb_stack_stamp.hpp
+++ include/gtirb_stack_stamp/gtirb_stack_stamp.hpp
@@ -6,6 +6,11 @@
 #include <string>
 
 namespace gtirb_stack_stamp {
+
+/// \class StackStamper
+///
+/// \brief This class handles the insertion of stack-stamps in the entry and
+/// exit blocks of functions, as a simple example of control-flow integrity.
 class StackStamper {
 public:
   StackStamper(gtirb::Context& Ctx_) : Ctx{Ctx_} {
@@ -24,14 +29,42 @@ public:
     ks_close(Keystone);
   }
 
+  /// \brief Insert a sequence of assembly instructions into a byte interval.
+  /// Unlike ByteInterval::insertBytes, it automatically updates the offsets
+  /// of anything that occurs after the insertion.
+  ///
+  /// \param BI The byte interval to insert into.
+  /// \param Offset The offset into the byte interval to insert code at.
+  /// \param InsnsStr A string representing a sequence of assembly instructions.
   void insertInstructions(gtirb::ByteInterval& BI, uint64_t Offset,
                           const std::string& InsnsStr);
+
+  /// \brief Insert stack-stamping instructions into the entrance block of a
+  /// function.
+  ///
+  /// \param FunctionId The UUID of the function we are stack-stamping. Used
+  /// to calculate hash values.
+  /// \param Block The code block to insert instructions into.
   void stackStampEntranceBlock(const gtirb::UUID& FunctionId,
                                gtirb::CodeBlock& Block);
+
+  /// \brief Insert stack-stamping instructions into the exit block of a
+  /// function, that is, right before a return statement.
+  ///
+  /// \param FunctionId The UUID of the function we are stack-stamping. Used
+  /// to calculate hash values.
+  /// \param Block The code block to insert instructions into.
   void stackStampExitBlock(const gtirb::UUID& FunctionId,
                            gtirb::CodeBlock& Block);
+
+  /// \brief Stack-stamp a function.
+  ///
+  /// \param M The module that contains the function.
+  /// \param FunctionId The UUID of the function to stack-stamp.
   void stackStampFunction(gtirb::Module& M, const gtirb::UUID& FunctionId);
 
+  /// \brief Is this code block an exit block; that is, does it end in a
+  /// return instruction?
   bool isExitBlock(const gtirb::CodeBlock& Block);
 
 private:
@@ -40,7 +73,14 @@ private:
   ks_engine* Keystone;
 };
 
+/// \brief Stack-stamps all functions in a module.
+///
+/// \param Ctx the context that the module was created from.
+/// \param M The module to stack-stamp.
 void stackStamp(gtirb::Context& Ctx, gtirb::Module& M);
+
+/// \brief Registers all auxillary data schema needed by this file.
+/// Call this function before any usages of the GTIRB API.
 void registerAuxDataSchema();
 
 } // namespace gtirb_stack_stamp
