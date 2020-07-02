c9e1e2d726e2020925da3e662282f4951e68346c
Brian Fairservice <bfairservice@grammatech.com>
2020-07-02 09:37:57 -0400

    Setup windows CI

--- tests/test_stack_stamp.py
+++ tests/test_stack_stamp.py
@@ -1,39 +1,54 @@
 import unittest
 import subprocess
 import os
+import platform
 
 
 class StackStampTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         os.chdir("tests")
-        args = ["make", "factorial", "-B"]
-        ec = subprocess.call(args)
-        return ec == 0
+        if platform.system() == "Linux":
+            args = ["make", "factorial", "-B"]
+            ec = subprocess.call(args)
+            return ec == 0
+        return True
 
     def setUp(self):
         self._test_file = "factorial"
+        if platform.system() == "Linux":
+            self._ddisasm = "ddisasm"
+        elif platform.system() == "Windows":
+            self._ddisasm = "ddisasm.exe"
         self.assertTrue(os.path.isfile(self._test_file))
 
     def test_1_invocation(self):
-        args = ["ddisasm", "factorial", "--ir", "factorial.gtirb"]
+        args = [self._ddisasm, self._test_file, "--ir", "factorial.gtirb"]
         ec = subprocess.call(args)
         self.assertEqual(ec, 0)
 
+        if platform.system() == "Linux":
+            python = "python3"
+        elif platform.system() == "Windows":
+            python = "python"
+
         args = [
-            "python3",
+            python,
             "-m",
             "gtirb_stack_stamp",
             "factorial.gtirb",
             "--outfile",
             "factorial.gtirb.stamp",
-            "--rebuild",
-            "factorial.stamp",
         ]
+        # On windows, gtirb-pprinter can't binary print an ELF file, so only do
+        # that part of the test on linux
+        if platform.system() == "Linux":
+            args += ["--rebuild", "factorial.stamp"]
         ec = subprocess.call(args)
         self.assertEqual(ec, 0)
 
-        args = ["./factorial.stamp", "10"]
-        output = subprocess.check_output(args)
-        self.assertEqual(output, b"Factorial(10)=3628800\n")
+        if platform.system() == "Linux":
+            args = ["./factorial.stamp", "10"]
+            output = subprocess.check_output(args)
+            self.assertEqual(output, b"Factorial(10)=3628800\n")
         return True
