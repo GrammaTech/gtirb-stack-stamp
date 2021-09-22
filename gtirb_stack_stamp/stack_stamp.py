5e4fb86733b8a046c0c8010f3311e2073b7c2b1a
Jonathan Dorn <jdorn@grammatech.com>
2021-09-22 22:24:19 +0000

    Switch to use a gtirb-rewriting pass

--- gtirb_stack_stamp/stack_stamp.py
+++ gtirb_stack_stamp/stack_stamp.py
@@ -12,63 +12,54 @@
 #
 import random
 import logging
-from gtirb_functions import Function
-from gtirb_capstone import RewritingContext
-from gtirb_capstone.instructions import GtirbInstructionDecoder
+
+from gtirb_rewriting import (
+    AllFunctionsScope,
+    BlockPosition,
+    FunctionPosition,
+    Pass,
+    PassManager,
+    Patch,
+    patch_constraints,
+)
 
 
-def get_function_stamp_value(func):
-    # Just choose a random value for the stamp values.  An option for a
-    # deterministic value is to take a hash of the function name or beginning
-    # EA.
-    random.seed(func.get_name())
-    return (random.randint(0, 2 ** 32), random.randint(0, 2 ** 32))
+class StampPass(Pass):
+    """Add stack stamping instructions to every function."""
 
+    def __init__(self):
+        pass
 
-def apply_stack_stamp(ir, logger=logging.Logger("null"), context=None):
-    logger.info("Preparing IR for rewriting...")
-
-    # Remove addresses from byte intervals, because some of them
-    # will grow as part of this operation, and we don't want them
-    # to overlap as a result.
-    for bi in ir.byte_intervals:
-        bi.address = None
-
-    # Stamp each function in the IR.
-    ctx = RewritingContext(ir) if context is None else context
-    for m in ctx.ir.modules:
-        functions = Function.build_functions(m)
-        for f in functions:
-            stamp_function(m, f, ctx, logger=logger)
-
-
-def stamp_function(module, func, ctx, logger=logging.Logger("null")):
-    logger.debug("\nStamping function: %s" % func.get_name())
-    if len(func.get_exit_blocks()) == 0:
-        logger.debug("- No function returns, skipping")
-        return
-    if len(func.get_entry_blocks()) == 0:
-        logger.debug("- No function entry blocks, skipping")
-        return
-
-    (w1, w2) = get_function_stamp_value(func)
-    asm = "xorl $0x{:X},(%rsp);".format(w1) + "xorl $0x{:X},4(%rsp);".format(
-        w2
-    )
-    encoding, count = ctx.ks.asm(asm)
-
-    logger.debug("- Entry blocks")
-    for b in func.get_entry_blocks():
-        ctx.modify_block_insert(module, b, encoding, 0, logger=logger)
-
-    logger.debug("- Exit blocks")
-    for b in func.get_exit_blocks():
-        # Find the offset of the last instruction, and insert our bytes just
-        # before.
-        offset = 0
-        instructions = tuple(
-            GtirbInstructionDecoder(module.isa).get_instructions(b)
+    def begin_module(self, module, functions, context):
+        """Register insertions and replacements for the given functions."""
+        # Stamp the return address at the entry point of every function.
+        context.register_insert(
+            AllFunctionsScope(FunctionPosition.ENTRY, BlockPosition.ENTRY),
+            Patch.from_function(self.get_function_stamp_value),
         )
-        for instruction in instructions[:-1]:
-            offset += instruction.size
-        ctx.modify_block_insert(module, b, encoding, offset, logger=logger)
+        # Stamp the return address at the exit of every function.
+        context.register_insert(
+            AllFunctionsScope(FunctionPosition.EXIT, BlockPosition.EXIT),
+            Patch.from_function(self.get_function_stamp_value),
+        )
+
+    @patch_constraints(clobbers_flags=True)
+    def get_function_stamp_value(self, context):
+        # Just choose a random value for the stamp values.  An option for a
+        # deterministic value is to take a hash of the function name or
+        # beginning EA.
+        random.seed(context.function.get_name())
+        w1 = random.randint(0, 2 ** 32)
+        w2 = random.randint(0, 2 ** 32)
+        # gtirb-rewriting saves the flags to the top of the stack, so we need
+        # to stamp at offsets 8 and 12 from the top.
+        return f"""
+            xorl $0x{w1:X},8(%rsp)
+            xorl $0x{w2:X},12(%rsp)
+        """
+
+
+def apply_stack_stamp(ir, logger=logging.Logger("null")):
+    manager = PassManager(logger=logger)
+    manager.add(StampPass())
+    manager.run(ir)
